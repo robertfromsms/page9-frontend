2fef1672d995a7146ce1eeec2628665087744ec5
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-09-12 13:14:54 -0400

    troubleshooting

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -35,7 +35,6 @@ class LoginForm extends React.Component {
       return data
     })
     .then(data => {
-      debugger
       if (data.jwt) {
         const jwt = data.jwt
         localStorage.setItem("jwt", jwt)

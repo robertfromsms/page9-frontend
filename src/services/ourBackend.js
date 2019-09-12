1c4d66040e4678898f11d4c83e89516164f9b2e2
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-09-12 11:50:29 -0400

    second published commit

--- src/services/ourBackend.js
+++ src/services/ourBackend.js
@@ -1,12 +1,13 @@
 const url = "http://localhost:3000/"
+const onlineUrl = "https://pacific-thicket-17763.herokuapp.com/"
 
 function genericGetFetch(urlAddition) {
-	return (fetch(url + urlAddition)
+	return (fetch(onlineUrl + urlAddition)
 	    .then(response => response.json()))
 }
 
 function genericNonGetFetch(urlAddition, configObj) {
-	return (fetch(url + urlAddition, configObj)
+	return (fetch(onlineUrl + urlAddition, configObj)
 	    .then(response => response.json()))
 }
 
